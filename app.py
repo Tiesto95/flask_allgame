fb74944dc3cd904278cd464aa9c20489e0a8fe12
tiesto95 <bes-zaurbaev@yandex.ru>
2023-03-27 23:56:43 +0300

    add models

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
 from flask import Flask
+from config import config
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__, template_folder='templates')
+app.config.from_object(config)
+db = SQLAlchemy(app)
+
+@app.route('/', methods=['GET'])
+def home():
+    pass
 
---
+++ models/models_db.py
@@ -0,0 +1,19 @@
+from app import db
+from sqlalchemy.orm import validates
+from datetime import date
+
+
+class Cat(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(120), nullable=False)
+    descr1 = db.Column(db.Text)
+    descr2 = db.Column(db.Text)
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    cat_id = db.Column(db.Integer, db.ForeIgnKey(['cat.id']), nullable=False, index=True)
+    cat = db.relatinship(Cat, foreign_keys=[cat.id, ])
+    code = db.Column(db.Text, nullable=False)
+    descr = db.Column(db.Text)
+    add_date = db.Column(db.Date, default=date.today(), nullable=False)
+    is_visable = db.Column(db.Boolean, default=True, nullable=False)
